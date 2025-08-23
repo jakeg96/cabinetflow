b34f59bc11250e91ea013b7a2561324752cf26f0
jakeg96 <jakepgreen6@gmail.com>
2025-08-23 19:41:30 +1000

    layout tweaks

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="mx-3 md:mx-5">
-    <header class="flex justify-between p-4 mx-3 md:mx-5 fixed top-0 left-0 right-0 z-10 border-b bg-white">
+    <header class="flex justify-between p-3 mx-3 md:mx-5 fixed top-0 left-0 right-0 z-10 border-b bg-white">
       <div class="flex gap-1 justify-center items-center">
-        <Icon icon="radix-icons:hamburger-menu" class="size-6 text-zinc-800 mr-3"/>
-        <Icon icon="heroicons:cube-transparent" class="size-8 text-zinc-800"/>
-        <p class="text-2xl font-bold italic">CabinetFlow</p>
+        <Icon icon="radix-icons:hamburger-menu" class="size-5 text-zinc-800 mr-2"/>
+        <Icon icon="heroicons:cube-transparent" class="size-7 text-zinc-800"/>
+        <p class="font-bold italic">CabinetFlow</p>
       </div>
       <Button class="hidden sm:visible"><Icon icon="material-symbols:add-2" class="size-4 text-white"/>Add Job</Button>
     </header>
-    <div class="mx-2 md:mx-4 mt-20">
+    <div class="mx-2 md:mx-4 mt-18">
       <WeeklyView/>
     </div>
   </div>
