1beeb201dcacced9576b243fcb21d48b839a5cb7
jakeg96 <jakepgreen6@gmail.com>
2025-08-25 18:59:04 +1000

    UI tweaks

--- src/components/WeeklyView.vue
+++ src/components/WeeklyView.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <div class="flex gap-3 mt-2">
-          <p class="text-3xl font-bold italic">Week</p>
-          <p class="text-3xl italic font-light">25-29</p>
+          <p class="text-3xl font-bold ">Week</p>
+          <p class="text-3xl  font-light">25-29</p>
         </div>
     <div class="flex flex-col lg:flex-row gap-2 mt-1">
       <div class="flex flex-col gap-1 w-full">
         <div class="flex gap-3 mt-2 ">
-          <p class="text-2xl font-bold italic">Monday</p>
-          <p class="text-2xl italic font-light">25</p>
+          <p class="text-2xl font-bold ">Monday</p>
+          <p class="text-2xl font-light">25</p>
         </div>
         <WeeklyCard />
         <WeeklyCard />
@@ -16,8 +16,8 @@
       </div>
       <div class="flex flex-col gap-1 w-full">
         <div class="flex gap-3 mt-2">
-          <p class="text-2xl font-bold italic">Tuesday</p>
-          <p class="text-2xl italic font-light">26</p>
+          <p class="text-2xl font-bold">Tuesday</p>
+          <p class="text-2xl font-light">26</p>
         </div>
         <WeeklyCard />
         <WeeklyCard />
@@ -26,23 +26,23 @@
       </div>
       <div class="flex flex-col gap-1 w-full">
         <div class="flex gap-3 mt-2">
-          <p class="text-2xl font-bold italic">Wednesday</p>
-          <p class="text-2xl italic font-light">27</p>
+          <p class="text-2xl font-bold">Wednesday</p>
+          <p class="text-2xl font-light">27</p>
         </div>
         <WeeklyCard />
       </div>
       <div class="flex flex-col gap-1 w-full">
         <div class="flex gap-3 mt-2">
-          <p class="text-2xl font-bold italic">Thursday</p>
-          <p class="text-2xl italic font-light">28</p>
+          <p class="text-2xl font-bold">Thursday</p>
+          <p class="text-2xl font-light">28</p>
         </div>
         <WeeklyCard />
         <WeeklyCard />
       </div>
       <div class="flex flex-col gap-1 w-full">
         <div class="flex gap-3 mt-2">
-          <p class="text-2xl font-bold italic">Friday</p>
-          <p class="text-2xl italic font-light">29</p>
+          <p class="text-2xl font-bold">Friday</p>
+          <p class="text-2xl font-light">29</p>
         </div>
         <WeeklyCard />
         <WeeklyCard />
